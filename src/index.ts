96ca983ccdd461a167096cff75eb74fe348f8b68
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-03-08 08:52:08 +0100

    fix(ci): init db with docker compose service

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
+import { PrismaClient } from '@prisma/client';
 
-const app = new Hono().use(cors());
+(async () => {
+  const $prisma = new PrismaClient();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
-});
+  await $prisma.$connect();
 
-const port = 3000;
-console.log(`Server is running on port ${port}`);
+  const app = new Hono().use(cors());
 
-serve({
-  fetch: app.fetch,
-  port,
-});
+  app.get('/', (c) => {
+    return c.text('Hello Hono!');
+  });
+
+  const port = 3000;
+  console.log(`Server is running on port ${port}`);
+
+  serve({
+    fetch: app.fetch,
+    port,
+  });
+})();
